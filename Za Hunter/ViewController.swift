946f25c99bb805bdaa0bfada76bb6effc6ad0a46
Sanya Jain <sanyajain@sanyas-macbook-pro.local>
2018-07-11 13:28:19 -0500

    Located nearby pizza places

--- Za Hunter/ViewController.swift
+++ Za Hunter/ViewController.swift
@@ -11,24 +11,50 @@ import MapKit
 import CoreLocation
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate {
 
     @IBOutlet weak var map: MKMapView!
     let locationManager = CLLocationManager()
+    var region = MKCoordinateRegion()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.requestWhenInUseAuthorization()
         map.showsUserLocation = true
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        map.delegate = self
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first!
+        let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let span = MKCoordinateSpanMake(0.025, 0.025)
+        region = MKCoordinateRegionMake(center, span)
+        map.setRegion(region, animated: true)
+        }
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = "pizza"
+        request.region = region
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            if let response = response {
+                for mapItem in response.mapItems {
+                    print(mapItem.name!)
+                }
+            }
+        }
+    }
+    
+    
+    
 
 }
 
