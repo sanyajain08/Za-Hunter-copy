1e5d803c1dd68254603a86a11c52d47a15ecc99a
Sanya Jain <sanyajain@sanyas-macbook-pro.local>
2018-07-11 14:27:12 -0500

    Added Directions button

--- Za Hunter/LocationDetailsViewController.swift
+++ Za Hunter/LocationDetailsViewController.swift
@@ -42,15 +42,14 @@ class LocationDetailsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onDirectionButtonTappedd(_ sender: Any) {
+        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeWalking]
+        MKMapItem.openMaps(with: [selectedMapItem], launchOptions: launchOptions)
+        
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
+    
+   
 
 }
