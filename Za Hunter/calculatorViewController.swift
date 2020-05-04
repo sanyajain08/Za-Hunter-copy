978240eccd2685b4db4957d93b689e7bf990bc74
Sanya Jain <sanyajain@sanyas-macbook-pro.local>
2020-05-04 20:14:22 +0530

    bvchjbj

---
+++ Za Hunter/calculatorViewController.swift
@@ -0,0 +1,41 @@
+//
+//  calculatorViewController.swift
+//  Za Hunter
+//
+//  Created by Sanya Jain on 02/05/20.
+//  Copyright © 2020 Sanya Jain. All rights reserved.
+//
+
+import UIKit
+
+class calculatorViewController: UIViewController {
+    
+    
+     @IBOutlet weak var name: UITextField!
+     @IBOutlet weak var data1: UITextField!
+     @IBOutlet weak var data2: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
